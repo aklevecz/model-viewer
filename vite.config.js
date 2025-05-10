7b9f1ba85a12652519444633a9ee4c5c7f959174
ariel klevecz <ariel@yaytso.art>
2025-05-09 17:04:23 -0700

    trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
+  plugins: [cloudflare()],
   server: {
-    open: true
-  }
+    open: true,
+  },
 });
